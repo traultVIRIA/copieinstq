3e2b75a1719d7d32206f529c74aa36b6d889d7ec
traultVIRIA <127193150+traultVIRIA@users.noreply.github.com>
2024-12-04 16:54:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization();
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
